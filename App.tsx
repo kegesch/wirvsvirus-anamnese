efb7f5ac5f0f894452ea0c1ab1a07705614165c7
Dennis <dennis.ostertag@gmx.de>
2020-03-20 22:04:39 +0100

    :art: added global shared style file

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './src/shared/styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textRed}>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
